b729c972c73ede7ae7f3a38fe242903a707766fb
Tyler Long <tyler4long@gmail.com>
2019-04-09 20:47:23 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const R = require('ramda')
+
+const emails = [
+  { title: 'aaa', date: 20190401, sender: 'test1@gmail.com' },
+  { title: 'bbb', date: 20190301, sender: 'test2@gmail.com' },
+  { title: 'ccc', date: 20190201, sender: 'test3@gmail.com' },
+]
+
+// search by title 'bbb'
+console.log(R.pipe(
+  R.filter(email => email.title.includes('ccc'))
+)(emails))
+
+// search by date range and sender
+console.log(R.pipe(
+  R.filter(email => email.date > 20190205), 
+  R.filter(email => email.sender === 'test1@gmail.com'), 
+)(emails))
